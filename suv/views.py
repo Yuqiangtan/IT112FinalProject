99a93155eeb8848fd6440b6ec49715f5c33a843a
yuqiangtan <yuqiangtan13@gmail.com>
2021-06-15 12:11:34 +0800

    commit

--- suv/views.py
+++ suv/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from .models import Inventory, VehicleType, Review
 # Create your views here.
 def index(request):
     return render(request,'suv/index.html')
+
+def inventory(request):
+    inventory_list=Inventory.objects.all()
+    return render(request,'suv/inventory.html',{'inventory_list':inventory_list})
